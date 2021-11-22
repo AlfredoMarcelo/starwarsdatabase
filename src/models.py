978bf893139edb0587cac5d02bdca6cee9c799ff
AlfredoMarcelo <alfredo.supanta.26@gmail.com>
2021-11-22 23:19:23 +0000

    Ejercicio terminado

--- src/models.py
+++ src/models.py
@@ -8,23 +8,67 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(10), nullable=False)
+    email = Column(String(10), nullable=False)
+    
+    password = Column (Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    userId = Column(Integer,ForeignKey('user.id'))
+
+class Libro(Base):
+    __tablename__ = 'libros'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    userId = Column(Integer,ForeignKey('post.id'))
+
+class Pelicula(Base):
+    __tablename__ = 'película'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    fecha_de_estreno = Column(String)
+    userId = Column(Integer,ForeignKey('post.id'))
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    especie = Column(String)
+    afiliacion = Column(String)
+    userId = Column(Integer,ForeignKey('post.id'))
+
+class Serie(Base):
+    __tablename__ = 'series'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    fecha_de_estreno = Column(String)
+    userId = Column(Integer,ForeignKey('post.id'))
+
+class Comentario(Base):
+    __tablename__ = 'Comentarios'
+    id = Column(Integer, primary_key=True)
+    comentario = (String)
+    autor_Id = Column(Integer,ForeignKey('user.id'))
+    post_Id = Column(Integer,ForeignKey('post.id'))
+
+
+
+
+
+    
+    
 
     def to_dict(self):
         return {}
